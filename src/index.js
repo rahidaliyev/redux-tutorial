69c4e81522d57c9650205bffdad108a60e708b1c
Rahid Aliyev <rahidaliyev18@gmail.com>
2022-08-02 11:56:32 +0400

    user changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux';
@@ -9,16 +8,18 @@ import userReducer  from './features/user';
 const store = configureStore({
 
   reducer: {
-    user: userReducer,
+    user: userReducer
   },
 
 });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+
 );
+
+
 
